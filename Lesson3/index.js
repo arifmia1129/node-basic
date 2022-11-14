3e1eb3d14aa8d3042283ba3dfff298da37d8368a
arifvtti <arif.vtti@gmail.com>
2022-11-14 09:46:20 +0600

    lesson3

---
+++ Lesson3/index.js
@@ -0,0 +1,20 @@
+const os = require("os");
+const path = require("path");
+
+// console.log(os.userInfo());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
+// const extensionName = path.extname("index.html");
+// console.log(extensionName);
+
+// const joinFile = path.join(__dirname + "/../views");
+// console.log(joinFile);
